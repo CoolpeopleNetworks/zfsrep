8bf1fa3ff73f674218b12b72ffc97aa40185a0fa
John W. Terrell <john@coolpeoplenetworks.com>
2020-07-14 14:21:26 -0700

    Added ability to specify host in datasets spec

--- zfsrep.bash
+++ zfsrep.bash
@@ -1,10 +1,15 @@
-declare -A DATASETS=( ["tank/Public"]="dozer/Public" 
-                      ["tank/Users"]="dozer/Users"
+declare -A DATASETS=( ["root@10.99.80.80:tank/backups"]="tank/backups/servers"
+                      ["root@10.99.80.80:tank/ipxe"]="tank/backups/ipxe"
+		      ["root@10.99.80.80:tank/tftpboot"]="tank/backups/tftpboot"
+                      ["root@10.99.80.80:tank/Media"]="tank/backups/Media"
+                      ["root@10.99.80.80:tank/Public"]="tank/backups/Public" 
+                      ["root@10.99.80.80:tank/Users"]="tank/backups/Users"
+		      ["root@10.99.80.80:tank/Photography"]="tank/backups/Photography"
+		      ["root@10.99.88.1:zones"]="tank/backups/headnode_zones"
 		      )
 
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-PEER=10.0.80.80
-PEER_USER=root
+PEER=""
 
 SNAPSHOT_PREFIX=rep
 
@@ -13,24 +18,24 @@ function log
    >&2 echo $@
 }
 
-function remote_command
+function local_command
 {
-   local REMOTECOMMAND="ssh $PEER_USER@$PEER \"$@\""
-   log $REMOTECOMMAND
-   eval $REMOTECOMMAND
+    local LOCAL_COMMAND=$@
+    log "[LOCAL] $LOCAL_COMMAND"
+    eval $LOCAL_COMMAND
 }
 
-function local_background_command
+function remote_command
 {
-   local COMMAND=$@
-   log $COMMAND
-   eval $COMMAND &
+    local REMOTECOMMAND="ssh $PEER \"$@\""
+    log "[REMOTE: $PEER] $@"
+    eval $REMOTECOMMAND
 }
 
 function create_replication_snapshot
 {
     local SNAPSHOT_NAME=$SNAPSHOT_PREFIX$TIMESTAMP
-    local COMMAND="zfs snapshot $1@$SNAPSHOT_NAME"
+    local COMMAND="zfs snapshot -r $1@$SNAPSHOT_NAME"
     remote_command $COMMAND
 
     echo $SNAPSHOT_NAME
@@ -44,42 +49,81 @@ function create_bookmark_from_snapshot
 
 function delete_replication_snapshot
 {
-    local COMMAND="zfs destroy $1@$2"
+    local COMMAND="zfs destroy -r $1@$2"
+    remote_command $COMMAND
+}
+
+function delete_replication_bookmark
+{
+    local COMMAND="zfs destroy $1"
     remote_command $COMMAND
 }
 
 function perform_full_replication
 {
     log "Performing full replication from $1 to $2..."
-    local SNAPSHOT_NAME="$(create_replication_snapshot $1 $SNAPSHOT_NAME)"
+    SNAPSHOT_DATASET=$1
+    SNAPSHOT_NAME="$(create_replication_snapshot $SNAPSHOT_DATASET)"
 
-    local_background_command "(nc -l -p 8024 | /opt/tools/bin/mbuffer -q -s 32M -m 1G | pv -rtab | zfs receive $2)"
-    remote_command "zfs send $1@$SNAPSHOT_NAME | mbuffer -q -s 32M -m 1G | nc -w 600 10.99.50.50 8024"
+    local COMMAND="ssh $PEER -c aes128-ctr -m hmac-sha1 \"zfs send -R $SNAPSHOT_DATASET@$SNAPSHOT_NAME \" | pv -rtab | zfs receive $2"
+    local_command $COMMAND
+    if [ $? -ne 0 ]; then
+        log "Failed to replicate $1 - exit code $?."
+        delete_replication_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME
+	exit
+    fi
 
-    create_bookmark_from_snapshot $1 $SNAPSHOT_NAME
-    delete_replication_snapshot $1 $SNAPSHOT_NAME
+    create_bookmark_from_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME
+    delete_replication_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME
 
     log "Replication from $1 to $2 complete."
 }
 
 function perform_incremental_replication
 {
-    log "Performing incremental replication from $1 to $2 using bookmark $BOOKMARK..."
+    log "Performing incremental replication from $1 to $2 using bookmark $3..."
+    SNAPSHOT_DATASET=$1
+    SNAPSHOT_NAME="$(create_replication_snapshot $SNAPSHOT_DATASET)"
+    BOOKMARK=$3
+
+    local COMMAND="ssh $PEER -c aes128-ctr -m hmac-sha1 \"zfs send -i $BOOKMARK $SNAPSHOT_DATASET@$SNAPSHOT_NAME \" | pv -rtab | zfs receive -F $2"
+    local_command $COMMAND
+    if [ $? -ne 0 ]; then
+        log "Failed to replicate $1 - exit code $?."
+        delete_replication_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME
+	exit
+    fi
+
+    delete_replication_bookmark $BOOKMARK
+    create_bookmark_from_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME
+    delete_replication_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME
+
+    log "Incremental replication from $1 to $2 complete."
 }
+
+function cleanup
+{
+    log "\nCTRL-C detected...deleting snapshot..."
+    delete_replication_snapshot $SNAPSHOT_DATASET $SNAPSHOT_NAME 
+}
+
+trap 'cleanup; exit' INT
 
 # Loop through all datasets
 for KEY in "${!DATASETS[@]}"; do
     log "Replicating $KEY to ${DATASETS[$KEY]}"
 
-    # First, get the most recent snapshot bookmark (if any)
-    BOOKMARKS=$(ssh $PEER_USER@$PEER "zfs list -t bookmark -H | grep $KEY | sort -r | head -1 | cut -f1")
-    if [[ -z "$BOOKMARKS" ]]; then
-        perform_full_replication $KEY ${DATASETS[$KEY]}
-    else
-        perform_incremental_replication $KEY ${DATASETS[$KEY]} $BOOKMARK
-    fi
+    IFS=':'
+    read -r -a PEERSPEC <<< "$KEY"
+    PEER=${PEERSPEC[0]}
+    DATASET=${PEERSPEC[1]}
 
-    # Kill any background jobs
-    kill $(jobs -p)
+    # First, get the most recent snapshot bookmark (if any)
+    BOOKMARK=$(ssh $PEER "zfs list -t bookmark -H | grep $DATASET | sort -r | head -1 | cut -f1")
+    if [[ -z "$BOOKMARK" ]]; then
+        perform_full_replication $DATASET ${DATASETS[$KEY]}
+    else
+        perform_incremental_replication $DATASET ${DATASETS[$KEY]} $BOOKMARK
+    fi
 done
 
